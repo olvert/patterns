5ca3794f0c2ef7da411fa73edf5604f2fdde8cc6
Olle <me@olle.io>
2020-08-16 20:48:04 +0200

    add canvas boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import Head from 'next/head';
 
-const Home = (): JSX.Element => (
-  <div>
-    <Head>
-      <title>Patterns</title>
-    </Head>
+const Home = (): JSX.Element => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = React.useState<CanvasRenderingContext2D | null>(null);
 
-    <main>
-      <h1>
-        Hello World
-      </h1>
-    </main>
+  React.useEffect(() => {
+    if (canvasRef.current === null) { return; }
+    const renderCtx = canvasRef.current.getContext('2d');
 
-    <footer>
-    </footer>
-  </div>
-);
+    if (renderCtx === null) { return; }
+    setContext(renderCtx);
+
+    // if (context) context.fillRect(5, 5, 2, 2);
+  }, [context]);
+
+  return (
+    <div>
+      <Head>
+        <title>Patterns</title>
+      </Head>
+
+      <main className="w-screen h-screen flex items-center justify-center bg-purple-200">
+        <canvas
+          id="canvas"
+          ref={canvasRef}
+          className="bg-purple-100"
+          width={500}
+          height={500}
+        >
+        </canvas>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  );
+};
 
 export default Home;
